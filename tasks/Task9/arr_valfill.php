34b7e012fefbd2b1215575492f25d2ce41def62f
GeoRiderBSU <geowriter@tut.by>
2017-07-04 15:59:33 +0300

    add task 9 massive filling

--- tasks/Task9/arr_valfill.php
+++ tasks/Task9/arr_valfill.php
@@ -1,9 +1,12 @@
 <?php
-$a=3;
-$b=1;
-fill($a, $b);
-function fill ($a, $b)
+$a=5; //размерность
+$b=2; //значение для заполнения
+arr_fill($a, $b);
+function arr_fill ($a, $b)
 {
-    $arr=array_fill(0, $a, $b);
-    echo "<pre>"; print_r($arr); "</pre>";
+    for ($i=0; $i<$a; $i++)
+    {
+        $arr[$i] = array_fill(0,$a,$b ) ;
+    }
+    echo "<pre>"; print_r($arr); "</pre>"; //вывод массива
 }
